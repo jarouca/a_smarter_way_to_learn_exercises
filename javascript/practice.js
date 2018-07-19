1afed43f6333faabd7f5f8ddc21da9718a1b5d4a
Jonathan Arouca <jonathanarouca@gmail.com>
2018-07-18 23:49:17 -0400

    add if statement

--- javascript/practice.js
+++ javascript/practice.js
@@ -10,3 +10,6 @@ var name = "Tom Bombadil";
 var greeting = "Hello, " + name + "!";
 alert(greeting);
 var bluesGuitar = prompt("What's your favorite guitar to play the blues on?", "Gibson ES-335");
+if (bluesGuitar === "Gibson ES-335") {
+  alert("Mine too!");
+}
